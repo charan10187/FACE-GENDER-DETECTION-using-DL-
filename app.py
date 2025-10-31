64f9a681b4cfba51443d9eb36281411270875cb0
Sri Charan Surepalli <charan10187@gmail.com>
2025-10-31 20:51:43 +0530

    changed the load method

--- app.py
+++ app.py
@@ -11,34 +11,10 @@ app = Flask(__name__)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # ✅ Load Vision Transformer Model
-MODEL_NAME = "google/vit-small-patch16-224-in21k"
-
-def download_file_from_google_drive(file_id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-    session = requests.Session()
-    response = session.get(URL, params={'id': file_id}, stream=True)
-    
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            response = session.get(URL, params={'id': file_id, 'confirm': value}, stream=True)
-            break
-
-    CHUNK_SIZE = 32768
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:
-                f.write(chunk)
-
-model_path = "vit_utkface2.pth"
-if not os.path.exists(model_path):
-    print("⬇️ Downloading model from Google Drive...")
-    file_id = "1vqddsGZ6B4O4bmoHo6ETujq8phPfOrkC" #Google Drive file ID
-    download_file_from_google_drive(file_id, model_path)
-    print("✅ Model download complete.")
-
-model = ViTForImageClassification.from_pretrained(MODEL_NAME, num_labels=2)
-model.load_state_dict(torch.load(model_path, map_location=device))
+MODEL_NAME = "nateraw/vit-age-classifier"
+model = ViTForImageClassification.from_pretrained(MODEL_NAME)
 model.to(device).eval()
+
 
 # ✅ Image transformation
 transform = transforms.Compose([
